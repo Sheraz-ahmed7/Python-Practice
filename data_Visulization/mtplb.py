1ef4761ce1c3a5baca9a616e620ddcea31287b7e
Sheraz-ahmed7 <sherazahmadarisar@gmail.com>
2026-05-03 16:52:38 +0500

    Customizing Line Style and Markers

--- data_Visulization/mtplb.py
+++ data_Visulization/mtplb.py
@@ -4,8 +4,9 @@ import numpy as np
 x=[1,2,3,4,5]
 y=[6,7,8,9,10]
 
-plt.plot(x,y)
+plt.plot(x,y, linestyle='--', marker='o', color='r')  # Line style, marker, and color
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title('Simple Line Plot')
+
 plt.show()
