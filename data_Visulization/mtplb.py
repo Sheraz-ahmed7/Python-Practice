22a1f137baf24e3ba74c7af5698009c9c56c5658
Sheraz-ahmed7 <sherazahmadarisar@gmail.com>
2026-05-03 16:57:09 +0500

    added multiple lined in one plot

--- data_Visulization/mtplb.py
+++ data_Visulization/mtplb.py
@@ -4,7 +4,11 @@ import numpy as np
 x=[1,2,3,4,5]
 y=[6,7,8,9,10]
 
-plt.plot(x,y, linestyle='--', marker='o', color='r')  # Line style, marker, and color
+y2 = [2,3,4,5,6]
+
+#plt.plot(x,y, linestyle='--', marker='o', color='r')  # Line style, marker, and color
+plt.plot(x,y, label='Linear')
+plt.plot(x,y2, label='Square')
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title('Simple Line Plot')
